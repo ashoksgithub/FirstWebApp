f77acb3edafb58597870df2255e0ce6ab3c7a32a
ashoksgithub <s_4as@yahoo.com>
2020-09-08 14:36:32 +0530

    modified

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
+using System.Collections.Specialized;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Promotions prom = new Promotions();
+            NameValueCollection prolist = null;
+            //Console.WriteLine(prom.repositoryUrls);
+            prolist = prom.list;
+            Console.ReadLine();
+        }
+    }
+
+    public class Promotions
+    {
+        //List<string,string> lt = null;
+        public NameValueCollection list = null;
+        public Promotions()
+        {
+            list = new NameValueCollection();
+            var promotions = ConfigurationManager.GetSection("PromotionsGroup/Promotions") as NameValueCollection;
+            //repositoryUrls = ConfigurationManager.AppSettings.AllKeys;
+            foreach (var key in promotions.AllKeys)
+            {
+                list.Add(key, promotions[key]);
+            }
         }
     }
 }
